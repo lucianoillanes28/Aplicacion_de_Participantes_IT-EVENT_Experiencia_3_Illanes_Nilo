aa6361bf2c6b6cbc89611a7922d178196ea98ca7
Luciano <luciano.illanes19@outlook.com>
2024-11-29 10:13:08 -0300

    Update Environmets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000'
-  //apiUrl: 'https://apitestdata-usuarios.onrender.com'
+  //apiUrl: 'http://localhost:3000'
+  apiUrl: 'https://apitestdata-usuarios-1.onrender.com'
 };
